75e6ffb92a7a887cf80869cd544b03f3df5ce092
Ellen ITBSS <ellenlius@itbss.ac.id>
2026-06-02 09:11:51 +0700

    Update header.php

--- header.php
+++ header.php
@@ -1,10 +1,39 @@
+<?php
+$currentPage = basename($_SERVER['PHP_SELF'], ".php");
+
+$cssFiles = [
+  'index' => 'beranda.css',
+  'produk' => 'produk.css',
+  'tentang' => 'tentang.css',
+  'karir' => 'karir.css',
+  'faq' => 'faq.css'
+];
+?>
 <!doctype html>
 <html lang="id">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Lima Jaya</title>
-    <link rel="stylesheet" href="css/<?= $cssFiles[$currentPage]; ?>.css" />
+    <title><?= switch($currentPage) {
+  case 'index':
+    echo 'Beranda | Lima Jaya';
+    break;
+  case 'produk':
+    echo 'Produk | Lima Jaya';
+    break;
+  case 'tentang':
+    echo 'Tentang | Lima Jaya';
+    break;
+  case 'karir':
+    echo 'Karir | Lima Jaya';
+    break;
+  case 'faq':
+    echo 'FAQ | Lima Jaya';
+    break;
+  defaut:
+    echo 'Lima Jaya';
+  } ?></title>
+    <link rel="stylesheet" href="css/<?= $cssFiles[$currentPage] ?? 'style.css'; ?>.css" />
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
