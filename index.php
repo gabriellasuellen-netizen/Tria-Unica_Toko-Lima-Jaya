42cff4df25b3f33a96fcbbacfcb5d5c9edc297f4
GABRIELLA SUELLEN <gabriellasuellen@itbss.ac.id>
2026-06-02 08:25:09 +0700

    Remove head section from index.php
    Removed the head section of the HTML document.

--- index.php
+++ index.php
@@ -1,24 +1,3 @@
-<!doctype html>
-<html lang="id">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Lima Jaya</title>
-    <link rel="stylesheet" href="css/beranda.css" />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
-    />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&family=Great+Vibes&display=swap"
-      rel="stylesheet"
-    />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=shopping_bag_speed"
-    />
-  </head>
-
   <body>
     <!-- NAVBAR -->
     <div class="navbar">
