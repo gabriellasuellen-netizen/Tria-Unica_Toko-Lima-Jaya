e1c3135aafc3ffa28e3516cf7569a3202c96f1d8
Ellen ITBSS <ellenlius@itbss.ac.id>
2026-06-02 09:20:01 +0700

    Refactor tentang.php to use PHP header inclusion
    Removed HTML structure and included PHP header.

--- tentang.php
+++ tentang.php
@@ -1,19 +1,6 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Tentang Lima Jaya</title>
-    <link rel="stylesheet" href="css/tentang.css" />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
-    />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&family=Great+Vibes&display=swap"
-      rel="stylesheet"
-    />
-  </head>
+<?php
+include "header.php";
+?>
   <body>
     <!-- NAVBAR -->
     <div class="navbar">
