580d3473670b3e12e9e33a8c4df58cfd52ddfbea
Ellen ITBSS <ellenlius@itbss.ac.id>
2026-06-02 08:55:24 +0700

    Update header.php

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Lima Jaya</title>
-    <link rel="stylesheet" href="css/beranda.css" />
+    <link rel="stylesheet" href="css/<?= $cssFiles[$currentPage]; ?>.css" />
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
