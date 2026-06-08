af691c7baf52e9a53cf9c707f28c2354e8052226
GabriellaS <gabriellasuellen@itbss.ac.id>
2026-06-08 15:35:52 +0700

    update login

--- login.php
+++ login.php
@@ -1,10 +1,26 @@
-<?php
-include "header.php";
-?>
-<section>
-  <form action="sv_login.php" method="post">
-    <input type="text" placeholder="username" name="username">
-    <input type="password" name="password" placeholder="password">
-    <button type="submit">login</button>
-  </form>     
-</section>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login Lima Jaya</title>
+  <link rel="stylesheet" href="css/login.css" />
+  <link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&family=Great+Vibes&display=swap"
+    rel="stylesheet">
+</head>
+
+<body>
+  <section>
+    <form action="sv_login.php" method="post">
+      <input type="text" placeholder="username" name="username">
+      <input type="password" name="password" placeholder="password">
+      <button type="submit">login</button>
+    </form>
+  </section>
+</body>
+
+</html>
