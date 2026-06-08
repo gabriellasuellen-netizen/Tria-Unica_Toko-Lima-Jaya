9a997cc0c8c4079f025d4259c17a64ec44e86737
GABRIELLA SUELLEN <gabriellasuellen@itbss.ac.id>
2026-06-08 20:57:21 +0700

    Update header.php

--- header.php
+++ header.php
@@ -65,28 +65,6 @@ $cssFiles = [
     "faq"     => "faq"
   ];
   ?>
-
-  <!doctype html>
-  <html lang="id">
-
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title><?= $titles[$currentPage] ?? "Lima Jaya" ?></title>
-
-    <link rel="stylesheet"
-      href="css/<?= $cssFiles[$currentPage] ?? "style" ?>.css">
-
-    <link rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&family=Great+Vibes&display=swap"
-      rel="stylesheet">
-  </head>
-
-  <body>
-
     <div class="navbar">
       <div class="logonav">Lima Jaya</div>
 
