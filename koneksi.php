9d4fecac0d3d634b0c53ac87747936023b9da097
Angel-0823 <angellietju@itbss.ac.id>
2026-06-08 19:59:30 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,1 +1,14 @@
+<?php
 
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "db_limajaya";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
+  die("Koneksi database gagal: " . mysqli_connect_error(();
+}
+
+?>
