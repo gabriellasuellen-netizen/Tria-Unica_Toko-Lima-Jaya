51099015622f4cf67d63728b5263d972efaf07cf
GabriellaS <gabriellasuellen@itbss.ac.id>
2026-06-26 20:58:31 +0700

    latihan implementasi keamanan

---
+++ admin/produk/tambah2.php
@@ -0,0 +1,155 @@
+<?php
+include "../security.php";
+include "../../koneksi.php";
+
+if (isset($_POST['simpan'])) {
+
+    $nama = trim($_POST['nama']);
+    $merek = trim($_POST['merek']);
+    $id_kategori = $_POST['id_kategori'];
+
+    if (
+        $nama == '' ||
+        $id_kategori == '' ||
+        empty($_FILES['gambar']['name'])
+    ) {
+
+        $error = "Semua field wajib diisi.";
+    } else {
+
+        $gambar = $_FILES['gambar']['name'];
+        $tmp = $_FILES['gambar']['tmp_name'];
+
+        $stmtKategori = mysqli_prepare(
+            $conn,
+            "SELECT * FROM kategori WHERE id_kategori = ?"
+        );
+
+        mysqli_stmt_bind_param(
+            $stmtKategori,
+            "i",
+            $id_kategori
+        );
+
+        mysqli_stmt_execute($stmtKategori);
+
+        $result = mysqli_stmt_get_result($stmtKategori);
+        $kategori = mysqli_fetch_assoc($result);
+
+        $folder = $kategori['nama_kategori'];
+
+        $folderUpload = "../../img/Produk-img/" . $folder;
+
+        if (!is_dir($folderUpload)) {
+            mkdir($folderUpload, 0777, true);
+        }
+
+        move_uploaded_file(
+            $tmp,
+            $folderUpload . "/" . $gambar
+        );
+
+        $pathDatabase = $folder . "/" . $gambar;
+
+        $stmt = mysqli_prepare(
+            $conn,
+            "INSERT INTO produk
+            (nama, merek, gambar, id_kategori)
+            VALUES (?, ?, ?, ?)"
+        );
+
+        mysqli_stmt_bind_param(
+            $stmt,
+            "sssi",
+            $nama,
+            $merek,
+            $pathDatabase,
+            $id_kategori
+        );
+
+        $query = mysqli_stmt_execute($stmt);
+
+        if ($query) {
+
+            header("Location: index.php");
+            exit;
+        } else {
+
+            $error = "Data gagal disimpan.";
+        }
+
+        mysqli_stmt_close($stmt);
+        mysqli_stmt_close($stmtKategori);
+    }
+}
+
+$kategori = mysqli_query(
+    $conn,
+    "SELECT * FROM kategori ORDER BY nama_kategori ASC"
+);
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Tambah Produk</title>
+    <link rel="stylesheet" href="../css/tambah.css">
+</head>
+
+<body>
+
+    <h1>Tambah Produk</h1>
+
+    <a href="index.php">Kembali</a>
+
+    <br><br>
+
+    <?php if (isset($error)) : ?>
+        <p style="color:red;">
+            <?= $error; ?>
+        </p>
+    <?php endif; ?>
+
+    <form method="POST" enctype="multipart/form-data">
+
+        <label>Nama Produk</label><br>
+        <input type="text" name="nama">
+        <br><br>
+
+        <label>Merek</label><br>
+        <input type="text" name="merek">
+        <br><br>
+
+        <label>Kategori</label><br>
+        <select name="id_kategori">
+
+            <option value="">
+                -- Pilih Kategori --
+            </option>
+
+            <?php while ($k = mysqli_fetch_assoc($kategori)) { ?>
+
+                <option value="<?= $k['id_kategori']; ?>">
+                    <?= $k['nama_kategori']; ?>
+                </option>
+
+            <?php } ?>
+
+        </select>
+
+        <br><br>
+
+        <label>Gambar</label><br>
+        <input type="file" name="gambar">
+        <br><br>
+
+        <button type="submit" name="simpan">
+            Simpan
+        </button>
+
+    </form>
+
+</body>
+
+</html>
